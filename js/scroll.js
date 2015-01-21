55972bd92fbd62ef87e46d9fe46776474ee42469
tom yuan <t1123425@gmail.com>
2015-01-21 23:48:17 +0800

    demo 1.5

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+$(function(){
+
+	$(window).bind('scroll resize', function(){
+		var $this = $(this);
+		var $this_Top=$this.scrollTop();
+
+		if($this_Top > 300){	
+			$('#nav-toggle').css({"background-color":"#18bc9c"});
+
+		}
+		if($this_Top < 300){
+			$('#nav-toggle').css({"background-color":"transparent"});
+		}
+
+	}).scroll();
+
+});
