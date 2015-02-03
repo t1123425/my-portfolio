5b26f37e0e295cf98c934682b25e6893eaf366ad
tom yuan <t1123425@gmail.com>
2015-02-04 05:21:18 +0800

    navbar color

--- js/scroll.js
+++ js/scroll.js
@@ -4,11 +4,16 @@ $(function(){
 		var $this = $(this);
 		var $this_Top=$this.scrollTop();
 
-		if($this_Top > 300){	
-			$('#nav-toggle').css({"background-color":"#18bc9c"});
+		if($this_Top > 500){	
+			$('.navbar').css({"background-color":"#18bc9c",
+							  "transition": "background-color 0.5s linear", 
+							"-moz-transition":"background-color 0.5s linear",  
+							"-webkit-transition":"background-color 0.5s linear", 
+							"-ms-transition":"background-color 0.5s linear" 
+								});
 		}
-		if($this_Top < 300){
-			$('#nav-toggle').css({"background-color":"transparent"});
+		if($this_Top < 500){
+			$('.navbar').css({"background-color":"transparent"});
 		}
 
 	}).scroll();
