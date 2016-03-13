326d7da117c09e5bf0f60cff271e5edeead6365e
tom yuan <t1123425@gmail.com>
2016-03-13 22:33:38 +0800

    add skill progress effect

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,10 @@
 $(function(){
+	var firstNum = 70;
+	var secondNum = 70;
+	var thridNum = 70;
+	var count = $('.value');
+
+
 
 	$(window).bind('scroll resize', function(){
 		var $this_Top=$(this).scrollTop();
@@ -16,7 +22,7 @@ $(function(){
 		$('html,body').animate({scrollTop:$('#header').position().top}, 800);
 		return false;
 	});	
-	$('.about').click(function(){ 
+	$('.about,.page-down').click(function(){ 
 		$('html,body').animate({scrollTop:$('#about').position().top}, 800);
 		return false;
 	});
@@ -28,9 +34,32 @@ $(function(){
 		$('html,body').animate({scrollTop:$('#contact').position().top}, 800);
 		return false;
 	});
-	$('.page-down').click(function(){ 
-		$('html,body').animate({scrollTop:$('#about').position().top}, 800);
-		return false;
+
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 1000){
+			// console.debug('123');
+			showProgress();
+		}
 	});
 
+	function showProgress(){
+		$('#firstValue').text(firstNum);
+		$('#secondValue').text(secondNum);
+		$('#thirdValue').text(thridNum);
+		$('.value').each(function () {
+		    $(this).prop('Counter',0).animate({
+		        Counter: $(this).text()
+		    }, {
+		        duration: 2000,
+		        easing: 'swing',
+		        step: function (now) {
+		            $(this).text(Math.ceil(now));
+		        }
+		    });
+		});
+
+		$('#firstValue').css('width',''+firstNum+'%');
+		$('#secondValue').css('width',''+secondNum+'%');
+		$('#thirdValue').css('width',''+thridNum+'%');
+	}
 });
