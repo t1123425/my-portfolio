b95544bd5171c0e3ddebab5f0311c51d08a6cd1a
tom yuan <t1123425@gmail.com>
2016-07-21 02:13:49 +0800

    add new layout and effect on 2016/7/21

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ $(function(){
 	var menu = $('.navbar');
 	var nav_toggle = $('#nav-toggle');
 	var menu_button = menu.find('li a');
-	var firstNum = 70;
-	var secondNum = 70;
-	var thridNum = 70;
+	// var firstNum = 70;
+	// var secondNum = 70;
+	// var thridNum = 70;
 	var count = $('.value');
+
 
 	
 	$('.home').click(function(){ 
@@ -33,32 +34,39 @@ $(function(){
 		$('#chContent').show().siblings('.infoContent').hide();
 	});
 
-	$(window).scroll(function(){
+	$(window).on('scroll',function(){
 		if($(this).scrollTop() > 1000){
-			// console.debug('123');
-			showProgress();
+			// $(this).off('scroll');
+			setTimeout(function(){
+				showProgress();
+			},500);
+			
+		}
+		if($(this).scrollTop() > 2000){
+			showOut();
 		}
 	});
 
 	function showProgress(){
-		// $('#firstValue').text(firstNum);
-		// $('#secondValue').text(secondNum);
-		// $('#thirdValue').text(thridNum);
-		// $('.value').each(function () {
-		//     $(this).prop('Counter',0).animate({
-		//         Counter: $(this).text()
-		//     }, {
-		//         duration: 2000,
-		//         easing: 'swing',
-		//         step: function (now) {
-		//             $(this).text(Math.ceil(now));
-		//         }
-		//     });
-		// });
+		$('#htmlValue').add('#cssValue').text(90);
+		$('#jsValue').text(80);
+		$('#nodeValue').add('#dbValue').text(60);
+		$('.value').each(function () {
+		    $(this).prop('Counter',0).animate({
+		        Counter: $(this).text()
+		    }, {
+		        duration: 2000,
+		        easing: 'swing',
+		        step: function (now) {
+		            $(this).text(Math.ceil(now));
+		        }
+		    });
+		});
 
-		$('#firstValue').css('width',''+firstNum+'%');
-		$('#secondValue').css('width',''+secondNum+'%');
-		$('#thirdValue').css('width',''+thridNum+'%');
+		$('.html').add('.css3').css('clip','rect(10px 150px 150px 0px)');
+		$('.Js').css('clip','rect(20px 150px 150px 0px)');
+		$('.nodeJS').add('.MongoDB').css('clip','rect(45px 150px 150px 0px)');
+		
 	}
 
 	//resize of background img
@@ -71,6 +79,7 @@ $(function(){
 
 	$(window).resize(function(){
 		backImgResize();
+		
 	});
 
 
@@ -83,32 +92,52 @@ $(function(){
 	//work show and hide 
 	$('.close-btn').click(function(){
 		$('.my-work').fadeOut();
-		$('.carousel').hide();
+		
 	});
 
-	function showOut(n){
-		if(n==1){
-			$('#Demo1-Carousel').show();
+	function popUp(n){
+		if(n == 1){
+			$('#demo1').show().siblings('.workContent').hide();
 			$('.my-work').fadeIn();
 		}else {
 			$('.my-work').fadeIn();
-			$('#Demo2-Carousel').show();
+			$('#demo2').show().siblings('.workContent').hide();
 		}
 
 	}
 	$('#portfolio1').click(function(){
-		showOut(1);
+		$('#demo1').show().siblings('.workContent').hide();
+		$('.my-work').fadeIn();
 	});
 	$('#portfolio2').click(function(){
-		showOut(2);
+		$('#demo2').show().siblings('.workContent').hide();
+		$('.my-work').fadeIn();
 	});
-	$.getJSON('js/infor.json',function(data){
-		console.log(data);
+	$('#portfolio3').click(function(){
+		$('#demo3').show().siblings('.workContent').hide();
+		$('.my-work').fadeIn();
 	});
+
+	// $.getJSON('js/infor.json',function(data){
+	// 	console.log(data);
+	// });
 	
 	//carousel slider 
 	$( ".carousel" ).carousel({ 
 		interval: 5000 
 	});
 
+	// work showout
+	function showOut(){
+		var workList = $('.portfolio-item');
+		a = 0;
+		for(i=0 ;i<workList.length;i++){
+			setTimeout(function(){
+				workList.eq(a).css('transform','scale(1)');
+				a++;
+			},500*i);
+		}
+
+	}
+	
 });
