2f82574f2a9964503b9288da403eee2501cc0d98
tom yuan <t1123425@gmail.com>
2016-07-21 15:37:11 +0800

    fix work function and work data

--- js/main.js
+++ js/main.js
@@ -2,27 +2,33 @@ $(function(){
 	var menu = $('.navbar');
 	var nav_toggle = $('#nav-toggle');
 	var menu_button = menu.find('li a');
-	// var firstNum = 70;
-	// var secondNum = 70;
-	// var thridNum = 70;
+
 	var count = $('.value');
 
 
 	
 	$('.home').click(function(){ 
 		$('html,body').animate({scrollTop:$('#header').position().top}, 800);
+		menu.removeClass('active');
+		nav_toggle.removeClass('active');
 		return false;
 	});	
 	$('.about,.page-down').click(function(){ 
 		$('html,body').animate({scrollTop:$('#about').position().top}, 800);
+		menu.removeClass('active');
+		nav_toggle.removeClass('active');
 		return false;
 	});
 	$('.work').click(function(){ 
 		$('html,body').animate({scrollTop:$('#portfolio').position().top}, 800);
+		menu.removeClass('active');
+		nav_toggle.removeClass('active');
 		return false;
 	});
 	$('.contact').click(function(){ 
 		$('html,body').animate({scrollTop:$('#contact').position().top}, 800);
+		menu.removeClass('active');
+		nav_toggle.removeClass('active');
 		return false;
 	});
 
@@ -35,14 +41,22 @@ $(function(){
 	});
 
 	$(window).on('scroll',function(){
-		if($(this).scrollTop() > 1000){
+		var winWidth = $(window).width();
+
+		if($(this).scrollTop() > 1000 &&  winWidth > 796){
 			// $(this).off('scroll');
 			setTimeout(function(){
 				showProgress();
 			},500);
 			
+		}else if($(this).scrollTop() > 1200 && winWidth < 426){
+			setTimeout(function(){
+				showProgress();
+			},500);
 		}
-		if($(this).scrollTop() > 2000){
+		if($(this).scrollTop() > 2000 &&  winWidth > 796){
+			showOut();
+		}else if($(this).scrollTop() > 2800 && winWidth < 426){
 			showOut();
 		}
 	});
@@ -92,40 +106,20 @@ $(function(){
 	//work show and hide 
 	$('.close-btn').click(function(){
 		$('.my-work').fadeOut();
-		
+		$('.websiteLink').html("");
 	});
 
-	function popUp(n){
-		if(n == 1){
-			$('#demo1').show().siblings('.workContent').hide();
-			$('.my-work').fadeIn();
-		}else {
-			$('.my-work').fadeIn();
-			$('#demo2').show().siblings('.workContent').hide();
-		}
-
-	}
-	$('#portfolio1').click(function(){
-		$('#demo1').show().siblings('.workContent').hide();
-		$('.my-work').fadeIn();
-	});
-	$('#portfolio2').click(function(){
-		$('#demo2').show().siblings('.workContent').hide();
-		$('.my-work').fadeIn();
-	});
-	$('#portfolio3').click(function(){
-		$('#demo3').show().siblings('.workContent').hide();
-		$('.my-work').fadeIn();
-	});
-
-	// $.getJSON('js/infor.json',function(data){
-	// 	console.log(data);
-	// });
+	
+	
+	
 	
 	//carousel slider 
-	$( ".carousel" ).carousel({ 
-		interval: 5000 
-	});
+
+		$( ".carousel" ).carousel({ 
+			interval:  false
+		});
+
+	
 
 	// work showout
 	function showOut(){
@@ -139,5 +133,51 @@ $(function(){
 		}
 
 	}
-	
+	//work loading
+	function workLoad(n,p){
+		var div ="";
+		var imgDiv = "";
+		for(i=0;i<p;i++){
+			imgDiv += "<div class='item'><img src='img/portfolio/work"+n+"/demo"+(i+1)+".png' class='img-responsive'></div>";
+			div += "<li data-target='#Demo-Carousel' data-slide-to="+i+"></li>";
+		}
+		$('.carousel-inner').html(imgDiv);
+		$('.carousel-indicators').html(div);
+		$('.carousel-inner').find('.item').eq(0).addClass('active');
+		$('.carousel-indicators li').eq(0).addClass('active');
+		$('#demo').show();
+		$('.my-work').fadeIn();
+	}
+
+	// get work content from JSON 
+	function getWorkData(n){
+		$.getJSON('js/infor.json',function(data){
+			$('.chInfo').text(data[n].ch);
+		    $('.enInfo').text(data[n].en);
+		});
+	}
+
+	$('#portfolio1').click(function(){
+		workLoad(1,3);
+		getWorkData(0);
+	});
+	$('#portfolio2').click(function(){
+		workLoad(2,3);
+		getWorkData(1);
+		$('.websiteLink').html("<a href='https://www.qmi.emome.net/' target='_blank' class='urlLink'>Visit site</a>");
+	});
+	$('#portfolio3').click(function(){
+		workLoad(3,4);
+		getWorkData(2);
+		
+	});
+	$('#portfolio4').click(function(){
+		workLoad(4,2);
+		getWorkData(3);
+		$('.websiteLink').html("<a href='http://www.nuibi168.com/Home/Index' target='_blank' class='urlLink'>Visit site</a>");
+	});
+	$('#portfolio5').click(function(){
+		workLoad(5,10);
+		getWorkData(4);
+	});
 });
