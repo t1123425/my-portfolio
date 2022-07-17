ec626bdcbf8545a19baf105abf1b3d8bd1cfa97a
tom yuan <t1123425@gmail.com>
2022-07-17 19:27:59 +0800

    add react_version branch & source code

---
+++ src/pages/Work.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line prettier/prettier
+import React, { memo } from 'react'
+
+const Work: React.FC = () => {
+  return (
+    <section>
+      <h1>Work</h1>
+    </section>
+  )
+}
+
+export default memo(Work);
