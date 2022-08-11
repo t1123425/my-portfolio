9d82756e5e3ad5c6ad500f0c578e5ea6271ced2a
tom yuan <t1123425@gmail.com>
2022-08-11 13:52:14 +0800

    add firbase moudle & storage api & add load api function & update env

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,34 @@ import { Navbar } from './components/Navbar'
 import { About } from './pages/About'
 import Home from './pages/Home'
 import Work from './pages/Work'
+import { initializeApp } from 'firebase/app'
+import { getStorage, ref, getDownloadURL } from 'firebase/storage'
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: `${process.env.REACT_APP_API_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.REACT_APP_API_ID}.firebaseio.com`,
+  projectId: process.env.REACT_APP_API_ID,
+  storageBucket: `${process.env.REACT_APP_API_ID}.appspot.com`,
+  messagingSenderId: process.env.REACT_APP_API_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+}
 
+// Initialize Firebase
+function LoadStorageImg() {
+  initializeApp(firebaseConfig)
+  const storage = getStorage()
+  const spaceRef = ref(storage, 'AppDownloadPlatform/demo1.png')
+  getDownloadURL(spaceRef).then((url) => {
+    console.log('url', url)
+  })
+}
 function LoadWorkData() {
   axios
-    .get('/databases/(default)/documents/work')
+    .get(process.env.REACT_APP_API_ID + '/databases/(default)/documents/work')
     .then((e) => {
       console.log('data', e)
+      LoadStorageImg()
     })
     .catch((err) => {
       console.error(err)
