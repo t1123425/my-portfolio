1ee025d21c151c6294d9beb62765da71f7d532cd
tom yuan <t1123425@gmail.com>
2022-09-22 22:25:53 +0800

    update mobile layout & html meta tag & add scroll effect & remove file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGripVertical, faCircleDown } from '@fortawesome/free-solid-svg-icons'
 const Home: React.FC = () => {
   const [active, setActive] = useState(false)
+  const [hide, setHide] = useState(false)
+  const scrollControl = () => {
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop
+    // const height =
+    //   document.documentElement.scrollHeight -
+    //   document.documentElement.clientHeight
+
+    // const scrolled = winScroll / height
+    // console.log(scrolled)
+    if (winScroll >= 25) {
+      setHide(true)
+    } else {
+      setHide(false)
+    }
+    console.log('height', winScroll)
+  }
   useEffect(() => {
+    window.addEventListener('scroll', scrollControl)
     setActive(true)
+    return () => {
+      window.removeEventListener('scroll', scrollControl)
+    }
   }, [])
   return (
     <Fragment>
-      <section className="Intro maxWidthContainer flex justifyCenter column">
+      <section className="viewHeight maxWidthContainer flex justifyCenter column pBottom50">
         <div className="showUpEffect">
           <h1 className={'item ' + (active && 'active')}>I'm Tom Yuan</h1>
         </div>
@@ -19,13 +40,20 @@ const Home: React.FC = () => {
             Front End Developer
           </h1>
         </div>
-        <div className="scrollDownIcon text-center floatEffect">
+        <div
+          className={
+            'scrollDownIcon text-center floatEffect ' + (!hide && 'active')
+          }
+        >
           <FontAwesomeIcon icon={faCircleDown} />
           <span>Scroll Down</span>
         </div>
       </section>
       <section className="maxWidthContainer pSide98">
-        {/* <p>Hi!there ,welcome to my portfolio, </p> */}
+        <div className="text-center infoText">
+          <p>Welcome to my portfolio,</p>
+          <p>let's take look at my recent works & side projects below.</p>
+        </div>
         <h1 className="title bold borderBottom text-center">
           My Recently Works
         </h1>
--- src/plugins/gsap.ts
+++
@@ -1,4 +0,0 @@
-import { gsap } from 'gsap'
-import { DrawSVGPlugin } from 'gsap-trial/DrawSVGPlugin'
-
-export const GsapSvg = gsap.registerPlugin(DrawSVGPlugin)
